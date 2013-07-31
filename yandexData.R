3c0224737ced383f2fa492865f4a17b96ea6904f
Yuri Buyanov <yuri.buyanov@gmail.com>
2013-07-31 13:06:16 +0400

    simplified and commented the whole program

--- yandexData.R
+++ yandexData.R
@@ -1,15 +1,15 @@
 require(XML)
 library(XML)
-xml.url <- "http://export.yandex.ru/last/last20x-static.xml"
-xmlfile <- xmlTreeParse(xml.url)
-root <- xmlRoot(xmlfile)
-requests <- xmlSApply(root[[1]], xmlValue)
-names(requests) <- NULL
+xmlfile <- xmlTreeParse("http://export.yandex.ru/last/last20x-static.xml")
+requests <- xmlSApply(xmlRoot(xmlfile)[[1]], xmlValue)
+
+#split and get plain words list (with duplicates)
 words <- tolower(unlist(sapply(requests, strsplit, " "), recursive=FALSE, use.names=FALSE))
-t <- table(words)
-freq <- as.data.frame(t, row.names=names(t))
-freq <- freq[, "Freq", drop=FALSE]
-freq <- freq[order(-freq[,1]), ,drop=FALSE]
-freq <- freq[grep("^[a-zа-я]{4,}$", row.names(freq)), ,drop=FALSE]
-h = head(freq, n = 10)
-barplot(h$Freq, names.arg=row.names(h))
+
+#sorted frequency table
+freq <- sort(table(words), decreasing=TRUE)
+
+#filter out short words and non-words
+freq <- freq[grep("^[a-zа-я]{4,}$", names(freq))]
+
+barplot(head(freq, n = 10))

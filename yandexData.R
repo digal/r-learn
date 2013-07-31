2fbea6b325c9235a79e111e2072dcae16d25795f
Yuri Buyanov <yuri.buyanov@gmail.com>
2013-07-31 12:47:48 +0400

    dataframe names

--- yandexData.R
+++ yandexData.R
@@ -6,8 +6,10 @@ root <- xmlRoot(xmlfile)
 requests <- xmlSApply(root[[1]], xmlValue)
 names(requests) <- NULL
 words <- tolower(unlist(sapply(requests, strsplit, " "), recursive=FALSE, use.names=FALSE))
-freq <- as.data.frame(table(words))
-freq <- freq[with(freq, order(-Freq)), ]
-freq <- freq[grep("^[a-zа-я]{4,}$", freq$words), ]
+t <- table(words)
+freq <- as.data.frame(t, row.names=names(t))
+freq <- freq[, "Freq", drop=FALSE]
+freq <- freq[order(-freq[,1]), ,drop=FALSE]
+freq <- freq[grep("^[a-zа-я]{4,}$", row.names(freq)), ,drop=FALSE]
 h = head(freq, n = 10)
-barplot(h$Freq, names.arg=h$words)
+barplot(h$Freq, names.arg=row.names(h))
